bffc44d2c4d1efacd3527de346cd1b1489a3a55e
KKomalHarne <89245111+KKomalHarne@users.noreply.github.com>
2021-08-24 21:36:31 +0530

    A.java

---
+++ A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class A
+{
+	public static void main(String args[])
+	{
+	Scanner s=new Scanner(System.in);
+	
+	System.out.println ("Enter a name");
+	
+	System.out.println("stringis []"+s);
+	}
+
+}
